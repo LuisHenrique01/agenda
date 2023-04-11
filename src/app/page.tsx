d300dcaec912560bd8c31e7a15cddd95aa8c5241
LuisHenrique01 <luizbueno1201@gmail.com>
2023-04-11 15:30:54 -0300

    Puxando da api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,21 +34,6 @@ export default async function Home() {
           </a>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
       <div className={styles.grid}>
         <ListAgenda
           items={agenda}
--- src/network/api.ts
+++ src/network/api.ts
@@ -1,8 +1,21 @@
 import { Agenda } from './../interfaces';
 import axios from 'axios';
 
+const API = axios.create({ baseURL: 'https://agenda-api-8nau.onrender.com/' })
+
 export async function list_agenda(): Promise<Agenda[]> {
-    return await axios.get('http://127.0.0.1:8000/')
+    return await API.get('')
+        .then(response => {
+            return response.data;
+        })
+        .catch(error => {
+            console.log(error);
+            return [];
+        });
+}
+
+export async function list_mock_agenda(): Promise<Agenda[]> {
+    return await API.get('/mock')
         .then(response => {
             return response.data;
         })
